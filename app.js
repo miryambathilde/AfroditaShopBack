1833ea7ed1a5e01b9afeba661f377b8c771e594b
Miryam <mbathilde@gmail.com>
2021-09-06 13:57:40 +0200

    instalamos cors para peticiones http

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const flash = require('connect-flash');
 const session = require('express-session');
+const cors = require('cors');
 
 // DOT ENV LIBRERIA //
 require('dotenv').config();
@@ -27,6 +28,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -50,12 +52,12 @@ app.use('/api', apiRouter);
 app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
